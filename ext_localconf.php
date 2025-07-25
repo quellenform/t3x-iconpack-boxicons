fab9b2ffb86eed0f2b4677338d3cc6d53a85fdb8
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2025-07-25 14:53:46 +0200

    Added option to modify iconpack styles

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,6 +9,7 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('iconpack')) {
         'EXT:iconpack_boxicons/Configuration/Iconpack/Boxicons2.yaml',
         \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
             \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-        )->get('iconpack_boxicons', 'configFile')
+        )->get('iconpack_boxicons', 'configFile'),
+        \Quellenform\IconpackBoxicons\Configuration\IconpackConfiguration::class
     );
 }
